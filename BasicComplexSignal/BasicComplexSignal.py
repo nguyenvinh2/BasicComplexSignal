c4edef1ec2eca294ed7300170ad224c64af82b39
nguye <nguyenv2@outlook.com>
2018-12-17 11:27:25 -0800

    added signal strength function

--- BasicComplexSignal/BasicComplexSignal.py
+++ BasicComplexSignal/BasicComplexSignal.py
@@ -1,17 +1,26 @@
 import numpy
 import scipy
+import matplotlib.pyplot as plt
 import os
 
 numpy.set_printoptions(threshold=numpy.nan)
-setPath = '../assets/assignment-01-2017.dat'
+setPath = '../assets/sample_data.dat'
 
 def computeSignal(setPath):
   dir = os.path.dirname(__file__)
   filename = os.path.join(dir, setPath)
-  dataComplex = numpy.fromfile(filename, dtype='int16').reshape(2048,2)
-  size = dataComplex.shape
-  print(dataComplex)
-  print(size)
+  dataComplex = numpy.fromfile(filename, dtype='int16')
+  dataComplex = 1.0*dataComplex[::2]+1.0j*dataComplex[1::2]
+  dataStrength = numpy.square(numpy.abs(dataComplex))
+  dataReshape = numpy.reshape(dataStrength, (16,128))
+  dataSize = numpy.size(dataReshape)
+  dataMean = numpy.mean(dataReshape,axis=0)
+  print(dataSize)
+  plt.plot(dataMean)
+  plt.title('Signal Strength of Returning Objects')
+  plt.xlabel('Range: x150m')
+  plt.ylabel('Signal Strength')
+  plt.show()
   return;
 
 computeSignal(setPath)
