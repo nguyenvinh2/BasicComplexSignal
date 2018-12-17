6c14cca625947c060909a6adf5c19c2d55233d19
nguye <nguyenv2@outlook.com>
2018-12-17 14:30:32 -0800

    added images

--- BasicComplexSignal/BasicComplexSignal.py
+++ BasicComplexSignal/BasicComplexSignal.py
@@ -15,19 +15,27 @@ def computeSignal(setPath):
   dataReshape = numpy.reshape(dataStrength, (16,128))
   dataSize = numpy.size(dataReshape)
   dataMean = numpy.mean(dataReshape,axis=0)
-  print(dataSize)
-
-  x=[0,1,3]
-  txt = [4,5,6]
 
   plt.title('Signal Strength of Returning Objects')
   plt.xlabel('Range: x150m')
   plt.ylabel('Signal Strength')
   plt.plot(dataMean)
-  plt.scatter(x,txt, marker='o')
   plt.show()
   return;
 
-def heatmapSignal(setPath);
+def heatmapSignal(setPath):
+  dir = os.path.dirname(__file__)
+  filename = os.path.join(dir, setPath)
+  dataComplex = numpy.fromfile(filename, dtype='int16')
+  dataComplex = 1.0*dataComplex[::2]+1.0j*dataComplex[1::2]
+  dataStrength = numpy.square(numpy.abs(dataComplex))
+  dataReshape = numpy.reshape(dataStrength, (16,128))
+  plt.imshow(dataReshape)
+  plt.title('Signal Strength over each Pulse')
+  plt.xlabel('Range: x150m')
+  plt.ylabel('Signal Strength')
+  plt.show()
+  return;
 
 computeSignal(setPath)
+heatmapSignal(setPath)
